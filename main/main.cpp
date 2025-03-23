018131354059c3c0573ce94d08abe15736d27526
saja fawagreh <saja.fawagreh@gmail.com>
2025-03-22 21:34:12 -0400

    Update main.cpp to support dynamic log file paths based on test config

--- main/main.cpp
+++ main/main.cpp
@@ -5,10 +5,12 @@
 #include <chrono>
 #include <fstream>
 #include <string>
+#include <filesystem>
 #include "include/plantPopulationCell.hpp"
 
 using namespace cadmium::celldevs;
 using namespace cadmium;
+namespace fs = std::filesystem;
 
 std::shared_ptr<GridCell<plantPopulationState, double>> addGridCell(const coordinates & cellId, const std::shared_ptr<const GridCellConfig<plantPopulationState, double>>& cellConfig) {
 	auto cellModel = cellConfig->cellModel;
@@ -22,19 +24,33 @@ std::shared_ptr<GridCell<plantPopulationState, double>> addGridCell(const coordi
 
 int main(int argc, char ** argv) {
 	if (argc < 2) {
-		std::cout << "Program used with wrong parameters. The program must be invoked as follows:";
-		std::cout << argv[0] << " SCENARIO_CONFIG.json [MAX_SIMULATION_TIME (default: 500)]" << std::endl;
+		std::cout << "Program used with wrong parameters. The program must be invoked as follows:" << std::endl;
+		std::cout << argv[0] << " SCENARIO_CONFIG.json [MAX_SIMULATION_TIME (default: 200)]" << std::endl;
 		return -1;
 	}
+
 	std::string configFilePath = argv[1];
-	double simTime = (argc > 2)? std::stod(argv[2]) : 200;
+	double simTime = (argc > 2) ? std::stod(argv[2]) : 200;
+
+	// Extract test name (e.g., "initialTest") from path
+	std::string testName = fs::path(configFilePath).parent_path().filename().string();
+
+	// Define log file paths
+	std::string gridLogPath = "log_files/" + testName + "/grid_log.csv";
+	std::string visLogPath = "log_files/" + testName + "/grid_visualization_log.csv";
 
 	auto model = std::make_shared<GridCellDEVSCoupled<plantPopulationState, double>>("plantPopulation", addGridCell, configFilePath);
 	model->buildModel();
-	
+
 	auto rootCoordinator = RootCoordinator(model);
-	rootCoordinator.setLogger<CSVLogger>("grid_log.csv", ";");
-	
+
+	// Choose logger based on config type
+	if (configFilePath.find("visualization") != std::string::npos) {
+		rootCoordinator.setLogger<CSVLogger>(visLogPath, ";");
+	} else {
+		rootCoordinator.setLogger<CSVLogger>(gridLogPath, ";");
+	}
+
 	rootCoordinator.start();
 	rootCoordinator.simulate(simTime);
 	rootCoordinator.stop();
